dda4fef1ee1abcd5ac08618f77a4a6d75eb9497c
Thaís Monteiro <77128802+thaxz@users.noreply.github.com>
2023-08-31 18:08:52 -0300

    first commit

---
+++ OnboardingTests/OnboardingTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingTestsApp.swift
+//  OnboardingTests
+//
+//  Created by thaxz on 31/08/23.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
